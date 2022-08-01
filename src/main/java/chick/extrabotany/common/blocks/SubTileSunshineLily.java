adbfac90631b9dddfdcb498bebf0db3591c1cec2
HuTao今天吃什么 <1184726415@qq.com>
2022-08-01 21:55:23 +0800

    v0.0.4-final

--- src/main/java/chick/extrabotany/common/blocks/SubTileSunshineLily.java
+++ src/main/java/chick/extrabotany/common/blocks/SubTileSunshineLily.java
@@ -3,6 +3,7 @@ package chick.extrabotany.common.blocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 import vazkii.botania.api.subtile.RadiusDescriptor;
@@ -13,6 +14,8 @@ import static chick.extrabotany.common.blocks.ModSubtiles.SUNSHINELILY;
 public class SubTileSunshineLily extends TileEntityGeneratingFlower
 {
     private static final int RANGE = 2;
+    private static final int DECAY_TIME = 200;
+    private int passiveDecayTicks;
 
     public SubTileSunshineLily(BlockPos pos, BlockState state)
     {
@@ -35,8 +38,18 @@ public class SubTileSunshineLily extends TileEntityGeneratingFlower
     public void tickFlower()
     {
         super.tickFlower();
-        if (getLevel().isDay())
+        if (getLevel().isDay() && !getLevel().isClientSide())
+        {
+            if (++passiveDecayTicks > DECAY_TIME)
+            {
+                getLevel().destroyBlock(getBlockPos(), false);
+                if (Blocks.DEAD_BUSH.defaultBlockState().canSurvive(getLevel(), getBlockPos()))
+                {
+                    getLevel().setBlockAndUpdate(getBlockPos(), Blocks.DEAD_BUSH.defaultBlockState());
+                }
+            }
             addMana(1);
+        }
     }
 
 

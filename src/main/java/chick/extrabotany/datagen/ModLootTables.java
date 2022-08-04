0c8d2773598b3b274500e231059133d0942db311
HuTao今天吃什么 <1184726415@qq.com>
2022-08-04 12:55:46 +0800

    v0.0.6

--- src/main/java/chick/extrabotany/datagen/ModLootTables.java
+++ src/main/java/chick/extrabotany/datagen/ModLootTables.java
@@ -20,8 +20,12 @@ public class ModLootTables extends BaseLootTableProvider
                 ModBlocks.YLG_ORE.get(),
                 createSilkTouchTable("yeluogui_ore", ModBlocks.YLG_ORE.get(), ModItems.RAW_YLG_CHUNK.get(), 1, 3)
         );
+        lootTables.put(ModSubtiles.omniviolet, createSimpleTable("omniviolet",ModSubtiles.omniviolet));
+        lootTables.put(ModSubtiles.omnivioletFloating, createSimpleTable("floating_omniviolet",ModSubtiles.omnivioletFloating));
 
-        //lootTables.put(ModSubtiles.sunshinelily, createSimpleTable("sunshine_lily",ModSubtiles.sunshinelily));
+        lootTables.put(ModSubtiles.serenitian, createSimpleTable("serenitian",ModSubtiles.serenitian));
+        lootTables.put(ModSubtiles.serenitianFloating, createSimpleTable("floating_serenitian",ModSubtiles.serenitianFloating));
+
         //lootTables.put(ModSubtiles.moonlightlily, createSimpleTable("moonlight_lily",ModSubtiles.moonlightlily));
         //lootTables.put(ModSubtiles.sunshinelilyFloating, createSimpleTable("floating_sunshine_lily",ModSubtiles.sunshinelilyFloating));
         //lootTables.put(ModSubtiles.moonlightlilyFloating, createSimpleTable("floating_moonlight_lily",ModSubtiles.moonlightlilyFloating));
